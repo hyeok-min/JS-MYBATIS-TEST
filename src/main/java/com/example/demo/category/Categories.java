ace37e1cf5af934328bced0dba750d0947f70402
hyeok-min <gurals04@gmail.com>
2023-12-23 22:47:05 +0900

    기본 준비

---
+++ src/main/java/com/example/demo/category/Categories.java
@@ -0,0 +1,5 @@
+package com.example.demo.category;
+
+public enum Categories {
+    Drama,Movie,Entertainment,Free,Qna;
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserMapper userMapper;
+
+    public List<User> getUserList(){
+        return userMapper.getUserList();
+    }
+}
